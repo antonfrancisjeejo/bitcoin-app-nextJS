5bae9e5597589499ce60fe9e62bffc38e6746a8e
unknown <anton13jeejo@gmail.com>
2020-10-22 16:59:47 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+
+const about = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Bitcoin price</h1>
+        <p>Application to view bitcoin prices</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default about;
